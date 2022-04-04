b0bf3744b5d6cc83e2746bc2d18d770fae1cae8d
omarhamada2321 <100031313+omarhamada2321@users.noreply.github.com>
2022-04-04 23:55:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,64 @@ provider "aws" {
 
 
 resource "aws_vpc" "main" {
-		cidr_block = "20.0.0.0/24"
+  cidr_block = "40.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "40.0.1.0/24"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "40.0.2.0/24"
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
+
+
+
+resource "aws_internet_gateway" "public-subnet-igw" {
+  vpc_id  = aws_vpc.main.id
+
+tags = {
+        Name = "public-subnet-igw"
+    }
+
+}
+
+
+resource "aws_route_table" "public-route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+     cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public-subnet-igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+
+resource "aws_route_table_association" "public-subnet1-association" {
+  subnet_id= aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public-route.id
+}
+
+
+
+resource "aws_route_table_association" "public-subnet2-association" {
+  subnet_id= aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public-route.id
 }
 
 terraform {
