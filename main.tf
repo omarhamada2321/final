39b6c6cdd59466aafc578573d74d032af0503011
omarhamada2321 <100031313+omarhamada2321@users.noreply.github.com>
2022-04-04 23:32:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,28 +4,14 @@ provider "aws" {
 
 
 resource "aws_vpc" "main" {
-		cidr_block = "10.0.0.0/24"
+		cidr_block = "20.0.0.0/24"
 }
 
 terraform {
   backend "s3" {
    region = "us-west-2"
-   key = "statefile"
+   key = "statefile/s3"
    bucket = "my524bucket5omar5hamdaa"
   }
 }
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Resource": "arn:aws:s3:::my524bucket5omar5hamdaa:"
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-      "Resource": "arn:aws:s3:::my524bucket5omar5hamdaa/path/to/my/key"
-    }
-  ]
-}
+
