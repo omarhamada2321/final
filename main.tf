dcfab43c7cb09877d2a6ba448e1502d7bdafceec
omarhamada2321 <100031313+omarhamada2321@users.noreply.github.com>
2022-04-04 22:50:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,18 @@ terraform {
    bucket = "my524bucket5omar5hamdaa"
   }
 }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::my524bucket5omar5hamdaa:"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+      "Resource": "arn:aws:s3:::my524bucket5omar5hamdaa/path/to/my/key"
+    }
+  ]
+}
