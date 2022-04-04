6eed769263415a572293d3ea13a02d68ceff069a
omarhamada2321 <100031313+omarhamada2321@users.noreply.github.com>
2022-04-05 01:00:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "aws_security_group" "public-sg" {
 
 }
 resource "aws_instance" "web" {
- ami = "ami-064ff912f78e3e561"
+ ami = "ami-00ee4df451840fa9d"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.public-sg.id]
   tags = {
